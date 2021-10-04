83e760736382e5ae536d07c15f78c176453871cd
oleksandr <oleksandrchervinskyi2@gmail.com>
2021-10-04 14:45:37 +0300

    finish all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
+import {Planes} from "./components/planes/planes";
+import {PlaneDetails} from "./components/planes/plane-details";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+const initialState = {
+    planes_list: [],
+    update_selector: 0
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SAVE_DATA':
+            return {
+                ...state,
+                planes_list: action.payload
+            }
+        case 'UPDATE_SELECTOR':
+            return {
+                ...state,
+                update_selector: state.update_selector + 1
+            }
+        default:
+            return state
+    }
+
+}
+
+const store = createStore(reducer)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Provider store={store}>
+            <Router>
+                <Switch>
+                    <Route path={'/planes/:id'} component={PlaneDetails}/>
+                    <Route path={'/planes'} component={Planes}/>
+                    <Route path={'/'} exact>
+                        <Redirect to="/planes"/>
+                    </Route>
+                </Switch>
+            </Router>
+        </Provider>
+    )
 }
 
 export default App;
